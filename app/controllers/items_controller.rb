198c3514ed23d75610b764e96e0f84eeefc4f453
Rebekah Shein <rmshein@gmail.com>
2015-11-27 15:06:35 -0500

    added create item method and private set_list and item_params methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,20 @@
 class ItemsController < ApplicationController
+  before_action :set_list
+
+  def create
+    @item = @list.items.create(item_params)
+
+    redirect_to @list
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def item_params
+    params.require(:item).permit(:content)
+  end
+
 end
